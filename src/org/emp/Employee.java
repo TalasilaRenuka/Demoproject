64a9f58dcf7daf41dc1f257bbe7fb7d6f667439a
talasilarenuka <talasilarenuka@gmail.com>
2020-02-18 14:47:11 +0530

    Project added

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,21 @@
+package org.emp;
+
+public class Employee {
+
+	public void empId()
+	{
+		System.out.println("EmpID is 123");
+	}
+	public void empName()
+	{
+		System.out.println("EmpName is renuka");
+	}
+	
+	public static void main(String args[])
+	{
+		Employee ep = new Employee();
+		ep.empId();
+		ep.empName();
+	}
+	
+}
